9695a2a54c1352b8b535b9db1484306dfdb42203
Sarah Stancik <skstancik@gmail.com>
2017-05-17 16:56:01 -0400

    debugging lack of vertical movement

--- js/controls.js
+++ js/controls.js
@@ -1,22 +1,28 @@
-var pos = document.getElementById('wasd-controls').getAttribute('position');
-
 window.addEventListener("keydown", function(event){
+  var pos = document.getElementById('view').getAttribute('position');
+  var cam = document.getElementById('view');
+
   if (event.defaultPrevented){
     return; // do nothing if the event was already processed
   }
 
   switch (event.key) {
+    // move up with spacebar " "
     case " ":
-      // move up with spacebar " "
-      y: pos.y ++;
-      element.setAttribute('position', pos.x+" "+pos.y+" "+pos.z);
+      console.log(pos);
+      pos.y ++;
+      console.log(cam);
+      cam.setAttribute('position', pos);
       console.log("up");
+      console.log(pos);
       break;
+    // move down with control "Control"
     case "Control":
-      // move down with control "Control"
-      y: pos.y --;
-      element.setAttribute('position', pos.x+" "+pos.y+" "+pos.z);
+      console.log(pos);
+      pos.y --;
+      cam.setAttribute('position', pos);
       console.log("down");
+      console.log(pos);
       break;
     default:
       return; // quit when this doesn't handle the key event
